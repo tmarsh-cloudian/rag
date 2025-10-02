5b93dc8f5509d49b26278644a403285a0d515ee4
Tom Marsh <tmarsh@cloudian.com>
2025-10-02 19:58:41 +0100

    debug

--- src/nvidia_rag/utils/configuration_wizard.py
+++ src/nvidia_rag/utils/configuration_wizard.py
@@ -216,7 +216,7 @@ class ConfigWizard(JSONWizard, YAMLWizard):  # type: ignore[misc] # dataclass-wi
             else:
                 full_envname = f"{ENV_BASE}{env_parent}_{envname}"
 
-            print(f"LOOKFORTHIS 1{ENV_BASE}1 2{env_parent}2 3{envname}3 4{full_envname}4 5{custom_env_name}5")
+            _LOGGER.info(f"LOOKFORTHIS 1{ENV_BASE}1 2{env_parent}2 3{envname}3 4{full_envname}4 5{custom_env_name}5")
 
             is_embedded_config = hasattr(val.type, "envvars")
 
